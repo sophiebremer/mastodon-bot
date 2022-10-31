859e511490b2efd9ddc02ce4b21ef6cc31c40f0a
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2022-10-31 22:48:25 +0100

    Add Client class

---
+++ lib/Client.d.ts
@@ -0,0 +1,18 @@
+import type Transformer from "./Transformer";
+export declare class Client {
+    static readonly types: Record<string, typeof Client>;
+    static get(type: string, ...args: Array<unknown>): Client;
+    constructor(...args: Array<unknown>);
+    getItems(date: Date): Array<Transformer.Item>;
+    getTimestamp(): Date;
+    setItems(items: Array<Transformer.Item>): void;
+}
+export declare namespace Client {
+    interface SourceConfig {
+        source_type: string;
+    }
+    interface TargetConfig {
+        target_type: string;
+    }
+}
+export default Client;
---
+++ sources/Client.ts
@@ -0,0 +1,102 @@
+
+/* *
+ *
+ *  Imports
+ *
+ * */
+
+import type Transformer from "./Transformer";
+
+/* *
+ *
+ *  Class
+ *
+ * */
+
+export class Client {
+
+    /* *
+     *
+     *  Static Properties
+     *
+     * */
+
+    public static readonly types: Record<string, typeof Client> = {};
+
+    /* *
+     *
+     *  Static Function
+     *
+     * */
+
+    public static get(
+        type: string,
+        ...args: Array<unknown>
+    ): Client {
+        return new Client.types[type](...args);
+    }
+
+    /* *
+     *
+     *  Constructor
+     *
+     * */
+
+    public constructor(
+        ...args: Array<unknown>
+    ) {
+        // nothing to do
+    }
+
+    /* *
+     *
+     *  Functions
+     *
+     * */
+
+    public getItems(date: Date): Array<Transformer.Item> {
+        throw new Error('Not implemented');
+    }
+
+    public getTimestamp(): Date {
+        throw new Error('Not implemented');
+    }
+
+    public setItems(items: Array<Transformer.Item>): void {
+        throw new Error('Not implemented');
+    }
+
+}
+
+/* *
+ *
+ *  Class Namespace
+ *
+ * */
+
+export namespace Client {
+
+    /* *
+     *
+     *  Declarations
+     *
+     * */
+
+    export interface SourceConfig {
+        source_type: string;
+    }
+
+    export interface TargetConfig {
+        target_type: string;
+    }
+
+
+}
+
+/* *
+ *
+ *  Default Export
+ *
+ * */
+
+export default Client;
