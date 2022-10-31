bcc37900dab88c5cfbc8696025fe7431f6ab1d66
sophiebremer <44302338+sophiebremer@users.noreply.github.com>
2022-11-01 00:03:22 +0100

    Add Transformer filter

--- sources/Transformer.ts
+++ sources/Transformer.ts
@@ -23,6 +23,9 @@ export class Transformer {
     public constructor(
         config: Transformer.Config
     ) {
+        this.negatives = config.negatives?.slice();
+        this.positives = config.positives?.slice();
+        this.replacements = { ...config.replacements };
         this.sourceClient = Client.get(config.source.source_type, config.source);
         this.targetClient = Client.get(config.target.target_type, config.target);
     }
@@ -32,6 +35,12 @@ export class Transformer {
      *  Properties
      *
      * */
+
+    protected negatives?: Array<string>;
+
+    protected positives?: Array<string>;
+
+    protected replacements?: Record<string, string>;
 
     protected sourceClient: Client;
 
@@ -43,13 +52,70 @@ export class Transformer {
      *
      * */
 
+    protected filter(
+        items: Array<Transformer.Item>
+    ): Array<Transformer.Item> {
+
+        if (
+            !this.negatives &&
+            !this.positives &&
+            !this.replacements
+        ) {
+            return items;
+        }
+
+        const filteredItems: Array<Transformer.Item> = [];
+        const negatives = this.negatives || [];
+        const positives = this.positives || [];
+        const replacements = this.replacements || {};
+
+        let negativesCounter: number;
+        let positivesCounter: number;
+        let text: string;
+
+        for (const item of items) {
+            negativesCounter = 0;
+            positivesCounter = 0;
+            text = item.text;
+
+            for (const negative of negatives) {
+                if (text.includes(negative)) {
+                    ++negativesCounter;
+                }
+            }
+
+            for (const positive of positives) {
+                if (text.includes(positive)) {
+                    ++positivesCounter;
+                }
+            }
+
+            if (negativesCounter > positivesCounter) {
+                continue;
+            }
+
+            for (const pattern in replacements) {
+                text = text.split(pattern).join(replacements[pattern]);
+            }
+
+            filteredItems.push({
+                ...item,
+                text
+            });
+        }
+
+        return filteredItems;
+    }
+
     public transform() {
         const sourceClient = this.sourceClient;
         const targetClient = this.targetClient;
 
-        const items = sourceClient.getItems(targetClient.getTimestamp());
-
-        targetClient.setItems(items);
+        targetClient.setItems(
+            this.filter(
+                sourceClient.getItems(targetClient.getTimestamp())
+            )
+        );
     }
 
 }
@@ -69,6 +135,8 @@ export namespace Transformer {
      * */
 
     export interface Config {
+        negatives?: Array<string>;
+        positives?: Array<string>;
         replacements?: Record<string, string>;
         source: Client.SourceConfig;
         target: Client.TargetConfig;    
